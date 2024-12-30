265b543d6fa61ab4cd6acae11af04c65a1c9a51d
larryyu2004 <jiayiyu@myyahoo.com>
2024-12-30 23:34:23 +1030

    day2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-// Parent passes Son
-// 1. Parent Component passes data
-// 2. Child Component receives data
+import { createContext, useContext, useState, useEffect } from "react"
 
-function Son(props){
-  console.log(props)
-  // props contains all the data from parent component
-  return <div>this is son, {props.name}</div>
-}
+//useState('')
 
-function App() {
-  const name = 'this is app name'
+function App() { 
+  if(Math.random() > 0.5){
+    useState('')
+  }
   return (
-    <div className="App">
-      <Son 
-      name={name}
-      age={18}
-      isTrue={false}
-      list={['vue', 'react']}
-      obj = {{ name: 'jask' }}
-      cb={() => console.log(123)}
-      child={<span>this is span</span>}
-      />
+    <div>
+
     </div>
   );
 }
